ae45521c5222a082ed8a5a7555b8fc150143ebe8
ngc7331 <ngc7331@outlook.com>
2023-08-05 11:13:21 +0800

    feat: use skopeo

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 login() {
     if [[ "$2" != null && "$3" != null ]]; then
         echo "-> login to $1"
-        docker login -u "$2" -p "$3" "$1"
+        skopeo login -u "$2" -p "$3" "$1"
         echo
     fi
 }
@@ -11,10 +11,12 @@ login() {
 logout() {
     if [[ "$2" != null ]]; then
         echo "-> logout from $1"
-        docker logout "$1"
+        skopeo logout "$1"
         echo
     fi
 }
+
+log="updater.log"
 
 l_url=`jq -r .local.url config.json`
 l_user=`jq -r .local.user config.json`
@@ -35,34 +37,12 @@ for ((i=0; i<$num; i++)); do
     repos=`python3 -c "import json; print(len(json.loads('''$data''')))"`
     for ((j=0; j<repos; j++)); do
         repo=`jq -r .remotes[$i].repos[$j] config.json`
-        echo "-> mirror $url/$repo"
-        echo "  -> pull"
-        docker pull "$url/$repo" | tee tmp
-        echo
-        if [[ `tail -2 tmp | grep "Image is up to date"` != "" ]]; then
-            echo "  -> up to date"
-            echo
-        else
-            echo $(date)                >> update_log
-            echo "-> mirror $url/$repo" >> update_log
-            echo "  -> pull"            >> update_log
-            cat  tmp                    >> update_log
-            if [[ `echo $repo | grep ":latest"` != "" ]]; then
-                echo "  -> remove old ones"                                  | tee -a update_log
-                docker rmi $(docker images | grep "none" | awk '{print $3}') | tee -a update_log
-                echo
-            fi
-            echo "  -> push"                                                 | tee -a update_log
-            docker tag "$url/$repo" "$l_url/$repo"                           | tee -a update_log
-            docker push "$l_url/$repo"                                       | tee -a update_log
-            echo                                                             | tee -a update_log
-        fi
-        echo
-        echo
+        echo "-> mirror $url/$repo" | tee -a $log
+#        skopeo delete docker://"$l_url"/"$repo"
+        skopeo copy docker://"$url"/"$repo" docker://"$l_url"/"$repo" | tee -a $log
     done
 
     logout "$url" "$user"
 done
 
-rm tmp
 logout "$l_url" "$l_user"
